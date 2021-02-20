1d550ffab48f3ecb85b4eefab03be9883efc5ce2
CESAR TZOC <csartalva01@gmail.com>
2021-02-20 03:50:48 -0600

    graph polygon

--- app/src/main/java/com/cesar31/figures/DrawPanel.java
+++ app/src/main/java/com/cesar31/figures/DrawPanel.java
@@ -10,6 +10,7 @@ import android.view.View;
 import androidx.annotation.RequiresApi;
 
 import com.cesar31.figures.graph.FigureContainer;
+import com.cesar31.figures.graph.Polygon;
 
 public class DrawPanel extends View {
 
@@ -20,6 +21,10 @@ public class DrawPanel extends View {
         this.container = container;
     }
 
+    /**
+     * OnDraw para dibujar
+     * @param canvas
+     */
     @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint("ResourceAsColor")
     protected void onDraw(Canvas canvas) {
@@ -28,55 +33,68 @@ public class DrawPanel extends View {
         paint.setStrokeWidth(5);
 
         this.container.getFour().forEach(f -> {
-            if(f.getType().equals("circulo")) {
+            if (f.getType().equals("circulo")) {
                 paint.setColor(getResources().getColor(getColor(f.getColor()), null));
                 canvas.drawCircle(f.getX(), f.getY(), f.getSide(), paint);
             }
 
-            if(f.getType().equals("cuadrado")) {
+            if (f.getType().equals("cuadrado")) {
                 paint.setColor(getResources().getColor(getColor(f.getColor()), null));
                 canvas.drawRect(f.getX(), f.getY(), f.getX() + f.getSide(), f.getY() + f.getSide(), paint);
             }
         });
 
         this.container.getFive().forEach(f -> {
-            if(f.getType().equals("rectangulo")) {
+            if (f.getType().equals("rectangulo")) {
                 paint.setColor(getResources().getColor(getColor(f.getColor()), null));
                 canvas.drawRect(f.getX(), f.getY(), f.getX() + f.getX2(), f.getY() + f.getY2(), paint);
             }
 
-            if(f.getType().equals("linea")) {
+            if (f.getType().equals("linea")) {
                 paint.setColor(getResources().getColor(getColor(f.getColor()), null));
                 canvas.drawLine(f.getX(), f.getY(), f.getX2(), f.getY2(), paint);
             }
         });
 
-        /*
-        canvas.save();
-        canvas.translate(0, -800);
-        canvas.scale(1f, 2f);
-        drawPolygon(canvas, paint);
-        canvas.restore();
-        drawPolygon(canvas, paint);
-         */
+        this.container.getPolygon().forEach(f -> {
+            drawPolygon(f, canvas, paint);
+            canvas.drawPoint(f.getX(), f.getY(), paint);
+        });
     }
 
-    public void drawPolygon(Canvas canvas, Paint paint) {
-        int h = 600, k = 800, r = 450, n = 6;
+    /**
+     * Metodo para dibujar poligonos
+     * @param p
+     * @param canvas
+     * @param paint
+     */
+    public void drawPolygon(Polygon p, Canvas canvas, Paint paint) {
+        float scale_y = (float) p.getHeight() / (float) p.getWidth();
+        canvas.save();
+        canvas.translate(p.getX(), p.getY());
+        canvas.rotate(270f);
+        canvas.scale(1f, scale_y);
+        int r = p.getWidth() / 2, n = p.getN();
         double rad = 2 * Math.PI / n;
-        canvas.drawPoint(h, k, paint);
+        paint.setColor(getResources().getColor(getColor(p.getColor()), null));
         for (int i = 0; i < n; i++) {
-            float x1 = h + r * (float) Math.cos(rad * i);
-            float y1 = k + r * (float) Math.sin(rad * i);
+            float x1 = r * (float) Math.cos(rad * i);
+            float y1 = r * (float) Math.sin(rad * i);
 
-            float x2 = h + r * (float) Math.cos(rad * (i + 1));
-            float y2 = k + r * (float) Math.sin(rad * (i + 1));
+            float x2 = r * (float) Math.cos(rad * (i + 1));
+            float y2 = r * (float) Math.sin(rad * (i + 1));
             canvas.drawLine(x1, y1, x2, y2, paint);
         }
+        canvas.restore();
     }
 
+    /**
+     * Metodo para obtener color segun el color de la figura
+     * @param clr
+     * @return
+     */
     private int getColor(String clr) {
-        switch(clr) {
+        switch (clr) {
             case "azul":
                 return R.color.blue;
             case "rojo":
